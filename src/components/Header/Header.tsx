86b0407d34f4ab0cc34561f77d518c8169190a5f
Voitenko Illia <79865476+IlyaVoitenko@users.noreply.github.com>
2024-10-15 22:42:26 +0200

    added mediaquery for laptop for the  header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,29 +6,34 @@ const Header = () => {
   return (
     <header className="containerHeader">
       <div className="containerInfoHeader">
-        <div className="logoContainer">
-          <span>Store</span>
-          <img src={logo} className="logoIcon" alt="logo" />
-          <span>mobile</span>
-        </div>
-        <span className="addressText">City name st. Street name, 21</span>
-        <div className="workScheduleContainer">
-          <div className="workSchedule">
-            <span className="workScheduleDays">Mon-Fri:</span>{" "}
-            <span className="workScheduleHours">10:00-19:00</span>{" "}
-            <span className="workScheduleDays">SAT:&emsp;&emsp;</span>
-            <span className="workScheduleHours">10:00-17:00</span>
+        <div className="containerLogoAndWorkSchedule">
+          <div className="logoContainer">
+            <span>Store</span>
+            <img src={logo} className="logoIcon" alt="logo" />
+            <span>mobile</span>
           </div>
-          <span className="workScheduleDays">SUN:&ensp;</span>{" "}
-          <span className="workScheduleHours">Output</span>{" "}
+          <span className="addressText">City name st. Street name, 21</span>
+          <div className="workScheduleContainer">
+            <div className="workSchedule">
+              <span className="workScheduleDays">Mon-Fri:</span>{" "}
+              <span className="workScheduleHours">10:00-19:00</span>{" "}
+              <span className="workScheduleDays">SAT:&emsp;&emsp;</span>
+              <span className="workScheduleHours">10:00-17:00</span>
+            </div>
+            <span className="workScheduleDays">SUN:&ensp;</span>{" "}
+            <span className="workScheduleHours">Output</span>{" "}
+          </div>
         </div>
-        <div className="searchInputContainer">
-          <img src={loop} alt="loop" className="loopImg" />
-          <input className="searchInput" placeholder="Search..." />
-        </div>
-        <div className="contactInfoContainer">
-          <span className="callBack">Call me back</span>
-          <span className="contactNumber">(099) 999-99-99</span>
+
+        <div className="containerSearchAndCallBack">
+          <div className="searchInputContainer">
+            <img src={loop} alt="loop" className="loopImg" />
+            <input className="searchInput" placeholder="Search..." />
+          </div>
+          <div className="contactInfoContainer">
+            <span className="callBack">Call me back</span>
+            <span className="contactNumber">(099) 999-99-99</span>
+          </div>
         </div>
       </div>
     </header>
