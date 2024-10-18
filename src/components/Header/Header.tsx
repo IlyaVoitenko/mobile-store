829f391fb167afc1cd5044c51985656f9df21b36
Voitenko Illia <79865476+IlyaVoitenko@users.noreply.github.com>
2024-10-18 22:39:42 +0200

    added media of header for mobile

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,37 +1,65 @@
+import { useState } from "react";
+
 import "./../../styles/layout/_header.scss";
 import logo from "/logo.svg";
 import loop from "../../assets/loop.svg";
 
 const Header = () => {
+  const [isShowMenu, setIsShowMenu] = useState<boolean>(false);
+  console.log(isShowMenu);
   return (
     <header className="containerHeader">
       <div className="containerInfoHeader">
         <div className="containerLogoAndWorkSchedule">
           <div className="logoContainer">
-            <span>Store</span>
-            <img src={logo} className="logoIcon" alt="logo" />
-            <span>mobile</span>
+            <div className="logoTextAndImage">
+              <span>Store</span>
+              <img src={logo} className="logoIcon" alt="logo" />
+              <span>mobile</span>
+            </div>
+            <button
+              className="btnShowMenu"
+              onClick={() => setIsShowMenu(!isShowMenu)}
+            >
+              &#9776;
+            </button>
           </div>
-          <span className="addressText">City name st. Street name, 21</span>
-          <div className="workScheduleContainer">
+          <span
+            className={`addressText ${
+              !isShowMenu ? "showMenu" : "notShowMenu"
+            }`}
+          >
+            City name st. Street name, 21
+          </span>
+          <div
+            className={`workScheduleContainer ${
+              !isShowMenu ? "showMenu" : "notShowMenu"
+            }`}
+          >
             <div className="workSchedule">
               <span className="workScheduleDays">Mon-Fri:</span>{" "}
               <span className="workScheduleHours">10:00-19:00</span>{" "}
-              <span className="workScheduleDays">SAT:&emsp;&emsp;</span>
-              <span className="workScheduleHours">10:00-17:00</span>
+              <span className="workScheduleDays ">SAT:</span>
+              <span className="workScheduleHours paddingLeft">10:00-17:00</span>
             </div>
             <span className="workScheduleDays">SUN:&ensp;</span>{" "}
             <span className="workScheduleHours">Output</span>{" "}
           </div>
         </div>
 
-        <div className="containerSearchAndCallBack">
+        <div
+          className={` containerSearchAndCallBack ${
+            !isShowMenu ? "showMenu" : "notShowMenu"
+          }`}
+        >
           <div className="searchInputContainer">
             <img src={loop} alt="loop" className="loopImg" />
             <input className="searchInput" placeholder="Search..." />
           </div>
           <div className="contactInfoContainer">
-            <span className="callBack">Call me back</span>
+            <a href="tel:(099) 999-99-99" className="callBack">
+              Call me back
+            </a>
             <span className="contactNumber">(099) 999-99-99</span>
           </div>
         </div>
