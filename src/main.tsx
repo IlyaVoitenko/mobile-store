483de8ec5d8a57dc9745863a7d8f9c2f2cfe5bbc
Voitenko Illia <79865476+IlyaVoitenko@users.noreply.github.com>
2025-01-05 16:05:08 +0100

    bugs fixed with adaptive

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import "./assets/styles/global.scss";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import "./index.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
