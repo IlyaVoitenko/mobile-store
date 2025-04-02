f943c0ea063123af99e7f4166275497051c61b99
Voitenko Illia <79865476+IlyaVoitenko@users.noreply.github.com>
2025-04-02 16:34:37 +0200

    added render all types filters for products

--- src/components/Category/Category.tsx
+++ src/components/Category/Category.tsx
@@ -1,3 +1,4 @@
+import { nanoid } from "nanoid";
 import { Link, useParams } from "react-router-dom";
 import "../../styles/pages/_index.scss";
 import Footer from "../Footer";
@@ -5,16 +6,17 @@ import Header from "../Header";
 import PriceRange from "../PriceRange";
 import ArrowRightGrey from "../../assets/ArrowRightGrey.svg";
 import arrowDownGrey from "../../assets/arrowDownGrey.svg";
+import arrowDownBlue from "../../assets/arrowDownBlue.svg";
 import "../../styles/components/_categoryNavProduct.scss";
 import "../../styles/components/_productNameAndCount.scss";
 import "../../styles/components/_filterAndListProducts.scss";
 import { filtersProduct } from "../../helper";
+import { useState } from "react";
 const Category = () => {
+  const [isShow, setIsShow] = useState(false);
   const { category } = useParams();
-  console.log(category, typeof category);
   const filters = filtersProduct(category);
-  console.log(filters);
-  const {} = filters || {};
+  const { models, memory, color, type } = filters || {};
   return (
     <div className="pageDefault">
       <Header />
@@ -47,6 +49,101 @@ const Category = () => {
             <div className="filterPrises">
               <span className="filterTitle">Price</span>
               <PriceRange />
+              {models && (
+                <div className="containerFilters">
+                  <span className="filterTitle">Phone models</span>
+                  <ul className={isShow ? "listFilterShow" : "listFilter"}>
+                    {models.map((item) => (
+                      <li key={nanoid()}>
+                        <label htmlFor={item} className="filterLabel">
+                          <input
+                            type="checkbox"
+                            id={item}
+                            name={item}
+                            className="checkboxFilter"
+                          />
+                          <span className="box"></span>
+
+                          <span className="filterSpan">{item}</span>
+                        </label>
+                      </li>
+                    ))}
+                  </ul>
+                  <button
+                    className="btnShowAll"
+                    onClick={() => setIsShow(!isShow)}
+                  >
+                    <span>Show all </span>
+                    <img src={arrowDownBlue} alt="" />
+                  </button>
+                </div>
+              )}
+              {memory && (
+                <div className="containerFilters">
+                  <span className="filterTitle">Memory</span>
+                  <ul className={"listFilterShow"}>
+                    {memory.map((item) => (
+                      <li key={nanoid()}>
+                        <label htmlFor={item} className="filterLabel">
+                          <input
+                            type="checkbox"
+                            id={item}
+                            name={item}
+                            className="checkboxFilter"
+                          />
+                          <span className="box"></span>
+
+                          <span className="filterSpan">{item}</span>
+                        </label>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+              {color && (
+                <div className="containerFilters">
+                  <span className="filterTitle">Color</span>
+                  <ul className={"listFilterShow"}>
+                    {color.map((item) => (
+                      <li key={nanoid()}>
+                        <label htmlFor={item} className="filterLabel">
+                          <input
+                            type="checkbox"
+                            id={item}
+                            name={item}
+                            className="checkboxFilter"
+                          />
+                          <span className="box"></span>
+
+                          <span className="filterSpan">{item}</span>
+                        </label>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+              {type && (
+                <div className="containerFilters">
+                  <span className="filterTitle">Type</span>
+                  <ul className={"listFilterShow"}>
+                    {type.map((item) => (
+                      <li key={nanoid()}>
+                        <label htmlFor={item} className="filterLabel">
+                          <input
+                            type="checkbox"
+                            id={item}
+                            name={item}
+                            className="checkboxFilter"
+                          />
+                          <span className="box"></span>
+
+                          <span className="filterSpan">{item}</span>
+                        </label>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
             </div>
           </div>
           <ul className="listProducts"></ul>
