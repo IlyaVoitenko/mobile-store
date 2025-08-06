678b3e34c6aee73f8c07e4e82032259b51e944f0
Voitenko Illia <79865476+IlyaVoitenko@users.noreply.github.com>
2025-08-06 21:53:42 +0200

    added viteImagemin configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { viteStaticCopy } from "vite-plugin-static-copy";
+import viteImagemin from "vite-plugin-imagemin";
 
 export default defineConfig({
   css: {
@@ -19,6 +20,37 @@ export default defineConfig({
           dest: "fonts",
         },
       ],
+    }),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      pngquant: {
+        quality: [0.65, 0.8],
+        speed: 3,
+      },
+      mozjpeg: {
+        quality: 75,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: "removeViewBox",
+            active: false,
+          },
+          {
+            name: "removeEmptyAttrs",
+            active: false,
+          },
+        ],
+      },
+      webp: {
+        quality: 75,
+      },
     }),
   ],
 });
